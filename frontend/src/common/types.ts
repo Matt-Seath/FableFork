2c79e1bc9a7c98a3eb6a07fa4cb38cd21c2b9c32
Matt Seath <seath.dev@gmail.com>
2023-04-11 21:57:13 +1000

    axios fetch in storyForm

--- frontend/src/common/types.ts
+++ frontend/src/common/types.ts
@@ -9,15 +9,15 @@ export interface Stats {
 
 export interface Character {
   mainCharacter: boolean;
-  role: string;
-  name: string;
-  age: number;
-  special: string;
-  stats: Stats;
+  role?: string;
+  name?: string;
+  age?: number;
+  special?: string;
+  stats?: Stats;
 }
 
 export interface StoryParams {
   genre: string;
   perspective: string;
-  characters: Character[];
+  characters?: Character[];
 }
--- frontend/src/components/Forms/StoryForm.tsx
+++ frontend/src/components/Forms/StoryForm.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { z } from "zod";
 import { StoryParams } from "../../common/types";
+import axios from "axios";
 
 const StoryForm = () => {
   const [storyParams, setStoryParams] = useState<StoryParams>({
     genre: "adventure",
     perspective: "firstPerson",
-    characters: [],
+  });
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => console.log(res.data[0].name));
   });
 
   console.log(storyParams);
