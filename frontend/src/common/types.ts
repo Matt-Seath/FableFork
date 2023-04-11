bd172743a79fff616a9fd9c902a3d586958d7e6c
Matt Seath <seath.dev@gmail.com>
2023-04-11 21:41:22 +1000

    added common/types

---
+++ frontend/src/common/types.ts
@@ -0,0 +1,23 @@
+export interface Stats {
+  random: boolean;
+  luck: number;
+  strength: number;
+  charisma: number;
+  intelligence: number;
+  dexterity: number;
+}
+
+export interface Character {
+  mainCharacter: boolean;
+  role: string;
+  name: string;
+  age: number;
+  special: string;
+  stats: Stats;
+}
+
+export interface StoryParams {
+  genre: string;
+  perspective: string;
+  characters: Character[];
+}
--- frontend/src/components/Forms/StoryForm.tsx
+++ frontend/src/components/Forms/StoryForm.tsx
@@ -1,36 +1,13 @@
 import React, { useState } from "react";
 import { z } from "zod";
-
-interface Stats {
-  random: boolean;
-  luck: number;
-  strength: number;
-  charisma: number;
-  intelligence: number;
-  dexterity: number;
-}
-
-interface Character {
-  mainCharacter: boolean;
-  role: string;
-  name: string;
-  age: number;
-  special: string;
-  stats: Stats;
-}
-
-interface StoryParams {
-  genre: string;
-  perspective: string;
-  characters: Character[];
-}
+import { StoryParams } from "../../common/types";
 
 const StoryForm = () => {
   const [storyParams, setStoryParams] = useState<StoryParams>({
     genre: "adventure",
     perspective: "firstPerson",
-    characters: []
-    });
+    characters: [],
+  });
 
   console.log(storyParams);
   return <div>{storyParams.perspective}</div>;
