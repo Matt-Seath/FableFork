de9625642fec9c3a195b41f4470f515594af7e38
matt seath <seath.dev@gmail.com>
2023-04-19 09:00:34 +1000

    refactored submit

--- frontend/src/components/Buttons/RadioButton.tsx
+++ frontend/src/components/Buttons/RadioButton.tsx
@@ -19,44 +19,56 @@ const RadioButton = ({
   onSelectItem,
 }: Props) => {
   const [input, setInput] = useState("");
+  const [disabled, setDisabled] = useState(false);
   const ref = useRef<HTMLInputElement>(null);
   return (
     <>
-      <li
-        className={
-          section === "submit"
-            ? "submit-button"
-            : selectedIndex === index
-            ? "radio-button-active"
-            : "radio-button"
-        }
-        key={index}
-        onClick={() => {
-          setSelectedIndex(index);
-          if (!inputFields.includes(children)) {
-            onSelectItem(section, children);
-          }
-        }}
-      >
-        {children}
-      </li>
-      {selectedIndex === index && inputFields.includes(children) && (
-        <input
-          ref={ref}
-          className="radio-button-input"
-          onChange={(event) => {
-            setInput(event.target.value);
-          }}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              onSelectItem(section, input);
-              ref.current!.blur();
+      {section !== "submit" ? (
+        <>
+          <li
+            className={
+              selectedIndex === index ? "radio-button-active" : "radio-button"
             }
+            key={index}
+            onClick={() => {
+              setSelectedIndex(index);
+              if (!inputFields.includes(children)) {
+                onSelectItem(section, children);
+              }
+            }}
+          >
+            {children}
+          </li>
+          {selectedIndex === index && inputFields.includes(children) && (
+            <input
+              ref={ref}
+              className="radio-button-input"
+              onChange={(event) => {
+                setInput(event.target.value);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  onSelectItem(section, input);
+                  ref.current!.blur();
+                }
+              }}
+              type="text"
+              id="other"
+              placeholder={`Type custom ${section} here and hit Enter:`}
+            />
+          )}
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            setDisabled(true);
+            onSelectItem(section, children);
           }}
-          type="text"
-          id="other"
-          placeholder={`Type custom ${section} here and hit Enter:`}
-        />
+          className="submit-button"
+          disabled={disabled}
+        >
+          Submit
+        </button>
       )}
     </>
   );
